53b3942a0ff44091b94d3c4097921c29e22e6b23
Pablo Cirant <cirant3002@gmail.com>
2019-09-09 14:14:50 -0300

    fix cert

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 export const firebaseConfig = {
   type: 'service_account',
   project_id: 'riplay-b89be',
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   client_email: 'firebase-adminsdk-zgplw@riplay-b89be.iam.gserviceaccount.com',
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: 'https://accounts.googlegoogleapis.com/o/oauth2/auth',
