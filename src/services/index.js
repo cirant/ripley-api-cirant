cd95c25d62b599689e952c8a0af9598fad4b860e
Pablo Cirant <cirant3002@gmail.com>
2019-09-10 23:26:49 -0300

    save data on redis

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,15 @@
 const axios = require('axios');
+import redis from 'redis';
+import dotenv from 'dotenv';
+dotenv.config();
+
+var client = redis.createClient(process.env.REDIS_SERVER, {
+  password: process.env.REDIS_PASSWORD
+}); //creates a new client 
+
+client.on('connect', function() {
+  console.log('redis connected');
+});
 
 export class Service {
   constructor() {
@@ -9,13 +20,32 @@ export class Service {
     return Number((Math.random() * 100).toFixed(0)) <= 15;
   }
 
-  async getproductDetails(sku){
+  getproductDetails(sku){
     if (this.simulateError()){
-      // TODO: guardar en redis el error
+      this.handlerError(sku, 'fail getting product by sku');
       return this.getproductDetails(sku);
     } else {
-      const { data } = await axios.get(`${this.riplayUrl}/products/${sku}`);
-      return data;
+
+      return new Promise((resolve, reject) => {
+        client.exists(sku, async(err, exist) => {
+          if (exist === 1) {
+            client.get(sku, (e, el)=> { 
+              return resolve(JSON.parse(el));
+            });
+          } else {
+              const { data } = await axios.get(`${this.riplayUrl}/products/${sku}`);
+              client.set(sku, JSON.stringify(data));
+              client.expire(sku, 120);
+              return resolve(data);
+          }
+        });
+      });
     }
+  }
+
+  handlerError(sku, message) {
+    const date = new Date();
+    const name = `error:product:${sku} ${message} on ${date}`;
+    client.rpush(['errors', name], redis.print);
   }
 }
